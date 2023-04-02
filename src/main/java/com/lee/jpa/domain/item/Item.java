5b05f012a3585afdf1eea13a8eb399e1218a063b
chanboklee <chanboklee@naver.com>
2023-04-03 00:23:43 +0900

    Fix : JPA - add item business logic

--- src/main/java/com/lee/jpa/domain/item/Item.java
+++ src/main/java/com/lee/jpa/domain/item/Item.java
@@ -2,6 +2,7 @@ package com.lee.jpa.domain.item;
 
 import com.lee.jpa.domain.Category;
 import com.lee.jpa.domain.OrderItem;
+import com.lee.jpa.exception.NotEnoughStockException;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -32,4 +33,23 @@ public abstract class Item {
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
 
+    // 비즈니스 로직
+
+    /**
+     * stock 증가
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * stock 감소
+     */
+    public void removeStock(int quantity){
+        int restStock = this.stockQuantity - quantity;
+        if(restStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
