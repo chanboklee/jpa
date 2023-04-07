67ec9e5d6bc029bf561e0bdd02572501ff94735f
chanboklee <chanboklee@naver.com>
2023-04-07 22:28:23 +0900

    Fix : JPA - add OrderSearch

---
+++ src/main/java/com/lee/jpa/repository/OrderSearch.java
@@ -0,0 +1,11 @@
+package com.lee.jpa.repository;
+
+import com.lee.jpa.domain.OrderStatus;
+import lombok.Getter;
+
+@Getter
+public class OrderSearch {
+
+    private String memberName;          // 회원 이름
+    private OrderStatus orderStatus;    // 주문 상태[ORDER, CANCEL]
+}
