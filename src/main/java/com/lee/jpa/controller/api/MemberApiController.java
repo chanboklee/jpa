56f75761ff70a36251abd569c51140e7fa0e608f
chanboklee <chanboklee@naver.com>
2023-04-09 12:38:28 +0900

    Fix : JPA - add member update api

--- src/main/java/com/lee/jpa/controller/api/MemberApiController.java
+++ src/main/java/com/lee/jpa/controller/api/MemberApiController.java
@@ -2,11 +2,10 @@ package com.lee.jpa.controller.api;
 
 import com.lee.jpa.domain.Member;
 import com.lee.jpa.service.MemberService;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import javax.validation.constraints.NotEmpty;
@@ -28,6 +27,28 @@ public class MemberApiController {
         Member member = new Member(request.getName());
         Long id = memberService.join(member);
         return new CreateMemberResponse(id);
+    }
+
+    @PutMapping("/api/v2/members/{id}")
+    public UpdateMemberResponse updateMemberV2(
+            @PathVariable("id") Long id,
+            @RequestBody @Valid UpdateMemberRequest request){
+
+        memberService.update(id, request.getName());
+        Member findMember = memberService.findOne(id);
+        return new UpdateMemberResponse(findMember.getId(), findMember.getUsername());
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class UpdateMemberResponse {
+        private Long id;
+        private String name;
+    }
+
+    @Data
+    static class UpdateMemberRequest {
+        private String name;
     }
 
     @Data
