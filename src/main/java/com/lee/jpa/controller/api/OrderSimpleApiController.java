e9b95ac4cbeb5a24a8599f48eef07b94af581154
chanboklee <chanboklee@naver.com>
2023-04-12 00:21:47 +0900

    Fix : JPA - order search api (entity exposure)

---
+++ src/main/java/com/lee/jpa/controller/api/OrderSimpleApiController.java
@@ -0,0 +1,29 @@
+package com.lee.jpa.controller.api;
+
+import com.lee.jpa.domain.Order;
+import com.lee.jpa.repository.OrderRepository;
+import com.lee.jpa.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+/**
+ * xToOne(ManyToOne, OneToOne)
+ * Order
+ * Order -> Member
+ * Order -> Delivery
+ */
+@RequiredArgsConstructor
+@RestController
+public class OrderSimpleApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/simple-orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        return all;
+    }
+}
