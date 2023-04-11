071859b335298e98f13a784b15b607ddcf097204
chanboklee <chanboklee@naver.com>
2023-04-12 00:58:38 +0900

    Fix : JPA - order search api (entity to dto)

--- src/main/java/com/lee/jpa/controller/api/OrderSimpleApiController.java
+++ src/main/java/com/lee/jpa/controller/api/OrderSimpleApiController.java
@@ -1,13 +1,18 @@
 package com.lee.jpa.controller.api;
 
+import com.lee.jpa.domain.Address;
 import com.lee.jpa.domain.Order;
+import com.lee.jpa.domain.OrderStatus;
 import com.lee.jpa.repository.OrderRepository;
 import com.lee.jpa.repository.OrderSearch;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * xToOne(ManyToOne, OneToOne)
@@ -25,5 +30,31 @@ public class OrderSimpleApiController {
     public List<Order> ordersV1(){
         List<Order> all = orderRepository.findAllByString(new OrderSearch());
         return all;
+    }
+
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        List<SimpleOrderDto> result = orders.stream()
+                .map(o -> new SimpleOrderDto(o))
+                .collect(Collectors.toList());
+        return result;
+    }
+
+    @Data
+    static class SimpleOrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getUsername();
+            orderDate = order.getOrderDate();
+            orderStatus = order.getStatus();
+            address = order.getDelivery().getAddress();
+        }
     }
 }
