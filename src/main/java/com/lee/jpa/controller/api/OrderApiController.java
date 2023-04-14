a26e443f06c65fded36d35d9acf4ad9ffb60eef2
chanboklee <chanboklee@naver.com>
2023-04-14 20:36:58 +0900

    Fix : JPA - optimize collection - entity exposure

---
+++ src/main/java/com/lee/jpa/controller/api/OrderApiController.java
@@ -0,0 +1,30 @@
+package com.lee.jpa.controller.api;
+
+import com.lee.jpa.domain.Order;
+import com.lee.jpa.domain.OrderItem;
+import com.lee.jpa.repository.OrderRepository;
+import com.lee.jpa.repository.OrderSearch;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RequiredArgsConstructor
+@RestController
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> ordersV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getUsername();
+            order.getDelivery().getAddress();
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o -> o.getItem().getName());
+        }
+        return all;
+    }
+}
